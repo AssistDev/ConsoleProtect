96a8a51e1e698a12bfe357f55bce2f3ca8194c19
soulofwolf <melorachabert@gmail.com>
2013-07-27 05:53:22 -0500

    - added incoding and initial password setting

--- src/me/avastprods/consoleprotect/Main.java
+++ src/me/avastprods/consoleprotect/Main.java
@@ -8,13 +8,37 @@ import org.bukkit.event.server.ServerCommandEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
-
+	private string pw = "":
+	private boolean pwSet = false;
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
 		getConfig().options().copyDefaults(true);
 		saveConfig();
+		pw = decode(getConfig().getString("rconpassword"));
+		pwSet = getConfig().getBoolean("passwordset");
 	}
-	
+	public void onDisable(){
+	    if (pwSet){
+		getConfig().set("rconpassword", encode(pw));
+		getConfig().set("passwordset", pwSet);
+		saveConfig();
+	    }
+	}
+	private String encode(String str) {
+            BASE64Encoder encoder = new BASE64Encoder();
+	    str = encoder.encodeBuffer(str.getBytes());
+            return str;
+	}
+
+    	private String decode(String str) {
+            BASE64Decoder decoder = new BASE64Decoder();
+	    try {
+                str = new String(decoder.decodeBuffer(str));
+	    } catch (IOException e) {
+        	e.printStackTrace();
+	    }
+            return str;
+	}
 	@EventHandler
 	public void onConsoleCommand(ServerCommandEvent e) {
 		CommandSender cmdSender = e.getSender();
@@ -26,13 +50,21 @@ public class Main extends JavaPlugin implements Listener {
 			e.setCommand("");
 			return;
 		} else {
+			if (!pwSet && args.length = 3){
+					if (args[1].equalsIgnoreCase("setPW"){
+						pw = args[2];
+						cmdSender.sendMessage("[ConsoleProtect] Password Saved");
+						pwSet = true;
+					}
+				}
 			if(args.length < 3) {
 				cmdSender.sendMessage("[ConsoleProtect] rcon <password> <command>");
 				return;
 			}
 			
 			if(args.length > 2) {
-				if(args[1].equals(getConfig().getString("rconpassword"))) {
+				
+				if(args[1].equals(pw)) {
 					String cmdToRun = StringUtils.join(args, ' ', 2, args.length);
 					cmdSender.sendMessage("[ConsoleProtect] Password correct! Executing command: " + cmdToRun);
 					e.setCommand(cmdToRun);
