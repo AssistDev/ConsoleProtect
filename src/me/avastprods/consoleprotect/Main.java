bd5002bba92c6538a8f5e39911aef5db8bbe4b82
AssistDev <avastprods@gmail.com>
2013-08-11 16:50:02 +0300

    Update Main.java

--- src/me/avastprods/consoleprotect/Main.java
+++ src/me/avastprods/consoleprotect/Main.java
@@ -6,10 +6,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerCommandEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
 public class Main extends JavaPlugin implements Listener {
-	private string pw = "":
+	private String pw = "";
 	private boolean pwSet = false;
+
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
 		getConfig().options().copyDefaults(true);
@@ -17,57 +19,57 @@ public class Main extends JavaPlugin implements Listener {
 		pw = decode(getConfig().getString("rconpassword"));
 		pwSet = getConfig().getBoolean("passwordset");
 	}
-	public void onDisable(){
-	    if (pwSet){
-		getConfig().set("rconpassword", encode(pw));
-		getConfig().set("passwordset", pwSet);
-		saveConfig();
-	    }
-	}
-	private String encode(String str) {
-            BASE64Encoder encoder = new BASE64Encoder();
-	    str = encoder.encodeBuffer(str.getBytes());
-            return str;
+
+	public void onDisable() {
+		if (pwSet) {
+			getConfig().set("rconpassword", encode(pw));
+			getConfig().set("passwordset", pwSet);
+			saveConfig();
+		}
 	}
 
-    	private String decode(String str) {
-            BASE64Decoder decoder = new BASE64Decoder();
-	    try {
-                str = new String(decoder.decodeBuffer(str));
-	    } catch (IOException e) {
-        	e.printStackTrace();
-	    }
-            return str;
+	private String encode(String str) {
+		str = Base64Coder.encodeString(str.toString());
+		System.out.println(str.charAt(2));
+		return str;
 	}
+
+	private String decode(String str) {
+		str = new String(Base64Coder.decode(str));
+		return str;
+	}
+
 	@EventHandler
 	public void onConsoleCommand(ServerCommandEvent e) {
 		CommandSender cmdSender = e.getSender();
 		String cmd = e.getCommand();
 		String[] args = cmd.split(" ");
-		
-		if(!cmd.startsWith("rcon")) {
+
+		if (!cmd.startsWith("rcon")) {
 			cmdSender.sendMessage("[ConsoleProtect] rcon <password> <command>");
 			e.setCommand("");
 			return;
+			
 		} else {
-			if (!pwSet && args.length == 3){
-					if (args[1].equalsIgnoreCase("setPW"){
-						pw = args[2];
-						cmdSender.sendMessage("[ConsoleProtect] Password Saved");
-						pwSet = true;
-					}
+			if (!pwSet && args.length == 3) {
+				if (args[1].equalsIgnoreCase("setpw")) {
+					pw = args[2];
+					cmdSender.sendMessage("[ConsoleProtect] Password saved");
+					pwSet = true;
 				}
-			if(args.length < 3) {
+			}
+			
+			if (args.length < 3) {
 				cmdSender.sendMessage("[ConsoleProtect] rcon <password> <command>");
 				return;
 			}
-			
-			if(args.length > 2) {
-				
-				if(args[1].equals(pw)) {
+
+			if (args.length > 2) {
+				if (args[1].equals(pw)) {
 					String cmdToRun = StringUtils.join(args, ' ', 2, args.length);
 					cmdSender.sendMessage("[ConsoleProtect] Password correct! Executing command: " + cmdToRun);
 					e.setCommand(cmdToRun);
+					
 				}
 			}
 		}
